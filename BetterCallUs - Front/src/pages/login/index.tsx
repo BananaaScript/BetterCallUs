5ac4dc2d340d3cabd11e146c52105d42792d2db9
Vitor Leandro <126176980+haikizinho@users.noreply.github.com>
2023-10-24 09:57:49 -0300

    organização dos css

--- BetterCallUs - Front/src/pages/login/index.tsx
+++ BetterCallUs - Front/src/pages/login/index.tsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/Auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import './syles/Login.css'
+
 
 export default function Login() {
   const [email, setEmail] = useState("");
