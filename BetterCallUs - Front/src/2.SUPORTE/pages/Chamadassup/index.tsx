992e465637dfc0157c02d4cbf9371b55222482ec
DouglasMedeiros1 <126176980+haikizinho@users.noreply.github.com>
2023-10-31 11:05:49 -0300

    arrumando o header

--- BetterCallUs - Front/src/2.SUPORTE/pages/Chamadassup/index.tsx
+++ BetterCallUs - Front/src/2.SUPORTE/pages/Chamadassup/index.tsx
@@ -1,49 +1,75 @@
+import React from 'react';
+import Login from '../../../pages/login';
+import { Link } from 'react-router-dom';
 import './styles/Ticket.css'
-import  Login from '../../../pages/login'
 
 export const Chamadassup = () => {
-    Login()
+  // Chame Login() aqui se necessário
 
-    return(
-        <div className='bodyTicket'>
-            <div className="sidenav">
-                <p className='linha'>
-                    <p>Chamada01</p>
-                    <p>(Não respondido)</p>
-                </p>
-                <hr></hr>
-                <p className='linha'>
-                    <p>Chamada02</p>
-                    <p>(Não respondido)</p>
-                </p>
-                <hr></hr>
-                <p className='linha'>
-                    <p>Chamada03</p>
-                    <p>(Não respondido)</p>
-                </p>
-                <hr></hr>
-                <p className='linha'>
-                    <p>Chamada04</p>
-                    <p>(Não respondido)</p>
-                </p>
-                <hr></hr>
-                <p className='linha'>
-                    <p>Chamada05</p>
-                    <p>(Não respondido)</p>
-                </p>
-                <hr></hr>
-                <p className='linha'>
-                    <p>Chamada06</p>
-                    <p>(Não respondido)</p>
-                </p>
-                <hr></hr>
-            </div>
-            <div className="main">
-                <h2>Sidenav Example</h2>
-                <p>This sidenav is always shown.</p>
-            </div>
-
+  return (
+    <div className='bodyTicket'>
+      <div className="container">
+        <div className="sidenav">
+          <p className='linha'>
+            <p>Chamada01</p>
+            <p>(Não respondido)</p>
+          </p>
+          <hr></hr>
+          <p className='linha'>
+            <p>Chamada02</p>
+            <p>(Não respondido)</p>
+          </p>
+          <hr></hr>
+          <p className='linha'>
+            <p>Chamada03</p>
+            <p>(Não respondido)</p>
+          </p>
+          <hr></hr>
+          <p className='linha'>
+            <p>Chamada04</p>
+            <p>(Não respondido)</p>
+          </p>
+          <hr></hr>
+          <p className='linha'>
+            <p>Chamada05</p>
+            <p>(Não respondido)</p>
+          </p>
+          <hr></hr>
+          <p className='linha'>
+            <p>Chamada06</p>
+            <p>(Não respondido)</p>
+          </p>
+          <hr></hr>
         </div>
-    )
 
+        <div className="main">
+          {/* Header específico para a página Chamadassup */}
+          <div className='headAPP'>
+            <div className='buttonAPP'>
+              <Link to='/ticket'><button>Enviar ticket</button></Link>
+            </div>
+            <div className='logoApp'>
+              <Link to='/'><img id='logo' alt='logo' src='logo.png' /></Link>
+            </div>
+            <div className='menuAPP'>
+              <button>
+                MENU
+              </button>
+            </div>
+            <Link to='/Chamadassup'><button>suporte</button></Link>
+            <Link to='/ticket'><button>admin</button></Link>
+            {/* Inclua outros elementos do cabeçalho conforme necessário */}
+          </div>
+
+          <h1>aaaaaaaa</h1>
+          <h1>aaaaaaaa</h1>
+          <h1>aaaaaaaa</h1>
+          <h1>aaaaaaaa</h1>
+          <h1>aaaaaaaa</h1>
+          <h1>aaaaaaaa</h1>
+          <h1>aaaaaaaa</h1>
+        </div>
+      </div>
+    </div>
+  )
 }
--- BetterCallUs - Front/src/App.tsx
+++ BetterCallUs - Front/src/App.tsx
@@ -1,62 +1,67 @@
+
 import React, { useContext } from 'react';
 import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';  
 import { RequireAuth } from './contexts/Auth/RequireAuth';
 import { AuthContext } from './contexts/Auth/AuthContext';
-import { Ticket } from './pages/Ticket'
+import { Ticket } from './pages/Ticket';
 import { Chamadassup } from './2.SUPORTE/pages/Chamadassup';
 
+function App() {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
 
-function App(){
-  const auth = useContext(AuthContext)
-  const navigate = useNavigate()
-
-  const handleLogout = async() =>{
-    await auth.logout()
-    navigate('/')
-    
+  const handleLogout = async () => {
+    await auth.logout();
+    navigate('/');
   }
+
+  const isChamadassupPage = window.location.pathname === '/Chamadassup';
 
   return (
     <div className='bodyAPP'>
-      
-      <div className='headAPP'>
-
+      {isChamadassupPage ? null : (
+        <div className='headAPP'>
           <div className='buttonAPP'>
             <Link to='/ticket'><button>Enviar ticket</button></Link>
-            
           </div>
-
           <div className='logoApp'>
-            <Link to='/'><img id='logo' alt='logo' src='logo.png'/></Link>
+            <Link to='/'><img id='logo' alt='logo' src='logo.png' /></Link>
           </div>
-
           <div className='menuAPP'>
             <button>
               MENU
             </button>
-
           </div>
-      </div>
-          <Link to='/Chamadassup'><button>suporte</button></Link>
+        </div>
+      )}
+      <Link to='/Chamadassup'><button>suporte</button></Link>
           <Link to='/ticket'><button>admin</button></Link>
-          <Link to='/edituser'><button>edit</button></Link>
           {auth.usuario && <button onClick={handleLogout}>Sair</button>}
-          
-          
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ticket" element={<RequireAuth><Ticket /></RequireAuth>} />
         <Route path="/Chamadassup" element={<RequireAuth><Chamadassup /></RequireAuth>} />
-        <Route path="/Menu/editaruser" element={<RequireAuth><menu /></RequireAuth>} />
       </Routes>
+
+      {isChamadassupPage && (
+        <div className="sidebar">
+          <p>safada</p>
+          <p>safada</p>
+        </div>
+      )}
+
+      {isChamadassupPage ? null : (
+        <footer>
+          <p>safada</p>
+          <p>safada</p>
+          <p>safada</p>
+          <p>safada</p>
+        </footer>
+      )}
     </div>
-    
   )
 }
 
-
-
 export default App;
-
