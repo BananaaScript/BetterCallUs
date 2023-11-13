312f5f01ac13d681c84a8ce4aea501272ab9bab4
BrunoFerCam <vitorleandrosoares@gmail.com>
2023-11-13 09:05:57 -0300

    FAQ até segurança

--- Sprint-4/BetterCallUs/src/app/src/pages/Home/index.tsx
+++ Sprint-4/BetterCallUs/src/app/src/pages/Home/index.tsx
@@ -10,68 +10,133 @@ export default class Home extends Component {
                 </div>
                 <div className='problemasHome'>
 
-                    <h3>Tipo de Problemas de rede</h3>
+                    <h3>Problemas de rede</h3>
 
                     <div className='botoes'>
                         <button className="quadrado">
                             <div className='iconebotao'></div>
-                            <div className='textobotao'>*******</div>
+                            <div className='textobotao'>Problema com conectividade na rede</div>
                         </button>
 
                         <button className="quadrado">
                             <div className='iconebotao'></div>
-                            <div className='textobotao'>*******</div>
+                            <div className='textobotao'>Latência Alta frequente</div>
                         </button>
 
                         <button className="quadrado">
                             <div className='iconebotao'></div>
-                            <div className='textobotao'>*******</div>
+                            <div className='textobotao'>Queda constante da internet</div>
                         </button>
 
                         <button className="quadrado">
                             <div className='iconebotao'></div>
-                            <div className='textobotao'>*******</div>
+                            <div className='textobotao'>Rede sobrecarregada</div>
                         </button>
 
                         <button className="quadrado">
                             <div className='iconebotao'></div>
-                            <div className='textobotao'>*******</div>
+                            <div className='textobotao'>Senha de rede esquecida</div>
                         </button>
 
                     </div>
                 </div>
                 <div className='problemasHome'>
-                    <h3>Tipo de Problemas de software</h3>
+                    <h3>Problemas de software</h3>
 
                     <div className='botoes'>
                     <button className="quadrado">
                         <div className='iconebotao'></div>
-                        <div className='textobotao'>*******</div>
+                        <div className='textobotao'>Sistema Operacional incompatível</div>
                     </button>
 
                     <button className="quadrado">
                         <div className='iconebotao'></div>
-                        <div className='textobotao'>*******</div>
+                        <div className='textobotao'>Problemas com atualização de Software</div>
                     </button>
 
                     <button className="quadrado">
                         <div className='iconebotao'></div>
-                        <div className='textobotao'>*******</div>
+                        <div className='textobotao'>Computador com desempenho lento</div>
                     </button>
 
                     <button className="quadrado">
                         <div className='iconebotao'></div>
-                        <div className='textobotao'>*******</div>
+                        <div className='textobotao'>como ter uma boa gestão de Armazenamento</div>
                     </button>
 
                     <button className="quadrado">
                         <div className='iconebotao'></div>
-                        <div className='textobotao'>*******</div>
+                        <div className='textobotao'>Problemas ao tentar abrir um programa</div>
                     </button>
                     </div>
                 </div>
                 <div className='problemasHome'>   
-                    <h3>Tipo de Problemas de segurança</h3>
+                    <h3>Problemas de segurança</h3>
+
+                    <div className='botoes'>
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>Como criar senhas fortes ?</div>
+                    </button>
+
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>Problemas de Firewall</div>
+                    </button>
+
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>Autenticação de dois fatores e outras formas de verificações</div>
+                    </button>
+
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>Invadiram minha conta, e agora ?</div>
+                    </button>
+
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>Segurança de rede</div>
+                    </button>
+                    
+                    </div>
+                </div>
+                
+                
+                <div className='problemasHome'>   
+                    <h3>Virus e Malware</h3>
+
+                    <div className='botoes'>
+
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>*******</div>
+                    </button>
+
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>*******</div>
+                    </button>
+
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>*******</div>
+                    </button>
+
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>*******</div>
+                    </button>
+
+                    <button className="quadrado">
+                        <div className='iconebotao'></div>
+                        <div className='textobotao'>*******</div>
+                    </button>
+
+                    </div>
+                </div>
+                <div className='problemasHome'>   
+                    <h3>Problemas de Hardware</h3>
 
                     <div className='botoes'>
                     <button className="quadrado">
@@ -102,12 +167,11 @@ export default class Home extends Component {
                     </div>
                 </div>
                 
-                
+
                 <div className='problemasHome'>   
-                    <h3>Respostas Comuns</h3>
+                    <h3>Problemas com Áudio e Video</h3>
 
                     <div className='botoes'>
-
                     <button className="quadrado">
                         <div className='iconebotao'></div>
                         <div className='textobotao'>*******</div>
@@ -132,9 +196,18 @@ export default class Home extends Component {
                         <div className='iconebotao'></div>
                         <div className='textobotao'>*******</div>
                     </button>
+                    
                     </div>
-            
                 </div>
+
+
+
+
+
+
+
+
+
             </>
         )
     }
