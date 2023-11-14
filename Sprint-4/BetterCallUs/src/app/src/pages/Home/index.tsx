aac0f75c668212f1e99f278faa533370bf17babe
haikizinho <vitorleandrosoares@gmail.com>
2023-11-14 10:36:52 -0300

    FAQ COMPLETAMENTE REFEITO

--- Sprint-4/BetterCallUs/src/app/src/pages/Home/index.tsx
+++ Sprint-4/BetterCallUs/src/app/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './styles/FAQ.css'
+import Botao from './botao';
+
 
 export default class Home extends Component {
     render(){
@@ -13,157 +15,148 @@ export default class Home extends Component {
                     <h3>Problemas de rede</h3>
 
                     <div className='botoes'>
-                        <button className="quadrado">
-                            <div className='iconebotao'></div>
-                            <div className='textobotao'>Problema com conectividade na rede</div>
-                        </button>
+                        
+                    <Botao textoBotao="Problemas com conectividade na rede" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                        <button className="quadrado">
-                            <div className='iconebotao'></div>
-                            <div className='textobotao'>Latência Alta frequente</div>
-                        </button>
+                    <Botao textoBotao="Latência alta frequente" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                        <button className="quadrado">
-                            <div className='iconebotao'></div>
-                            <div className='textobotao'>Queda constante da internet</div>
-                        </button>
+                    <Botao textoBotao="Queda constante da internet" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                        <button className="quadrado">
-                            <div className='iconebotao'></div>
-                            <div className='textobotao'>Rede sobrecarregada</div>
-                        </button>
+                    <Botao textoBotao="Rede sobrecarregada" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                        <button className="quadrado">
-                            <div className='iconebotao'></div>
-                            <div className='textobotao'>Senha de rede esquecida</div>
-                        </button>
+                    <Botao textoBotao="Senha de rede esquecida" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
                     </div>
+
                 </div>
+
+
+
+
                 <div className='problemasHome'>
                     <h3>Problemas de software</h3>
 
                     <div className='botoes'>
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Sistema Operacional incompatível</div>
-                    </button>
+                        
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Problemas com atualização de Software</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Computador com desempenho lento</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>como ter uma boa gestão de Armazenamento</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Problemas ao tentar abrir um programa</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
+
                     </div>
                 </div>
+
+
+
                 <div className='problemasHome'>   
                     <h3>Problemas de segurança</h3>
 
                     <div className='botoes'>
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Como criar senhas fortes ?</div>
-                    </button>
+                        
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Problemas de Firewall</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Autenticação de dois fatores e outras formas de verificações</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Invadiram minha conta, e agora ?</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Segurança de rede</div>
-                    </button>
-                    
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
+
                     </div>
                 </div>
                 
-                
+    
                 <div className='problemasHome'>   
                     <h3>Virus e Malware</h3>
 
                     <div className='botoes'>
+                        
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Como saber se meu computador está com virus ?</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Como se proteger contra virus e malwares</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Como livrar meu computador de virus</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Como virus podem afetar meu computador?</div>
-                    </button>
-
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>O que pode causar virus no meu computador?</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
                     </div>
                 </div>
+
+
+
                 <div className='problemasHome'>   
                     <h3>Problemas de Hardware</h3>
 
                     <div className='botoes'>
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Computador não está ligando</div>
-                    </button>
+                        
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Teclado/Mouse não estão funcionando/Falhas de usb</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Falha no HD</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>O computador está aquecendo demais</div>
-                    </button>
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
 
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>O computador está fazendo muito barulho</div>
-                    </button>
-                    
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
+
                     </div>
                 </div>
                 
@@ -171,41 +164,31 @@ export default class Home extends Component {
                 <div className='problemasHome'>   
                     <h3>Problemas com Áudio e Video</h3>
 
-                    <div className='botoes'>
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>A tela não acende quando o computador liga.</div>
-                    </button>
-
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>Dispositivos bluetooth não conectando.</div>
-                    </button>
-
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>O som não está saindo.</div>
-                    </button>
-
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'></div>
-                    </button>
-
-                    <button className="quadrado">
-                        <div className='iconebotao'></div>
-                        <div className='textobotao'>*******</div>
-                    </button>
                     
+                    <div className='botoes'>
+                        
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
+
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
+
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
+
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
+
+                    <Botao textoBotao="Clique Aqui 1" conteudo={
+                    <div>Conteúdo do Botão 1</div>
+                    } />
+
                     </div>
                 </div>
-
-
-
-
-
-
-
 
 
             </>
