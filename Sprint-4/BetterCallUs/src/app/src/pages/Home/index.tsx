9afa1ccf71a54c34d0c20085f4389cf80390b65a
haikizinho <vitorleandrosoares@gmail.com>
2023-11-14 11:35:53 -0300

    arrumando textos do FAQ

--- Sprint-4/BetterCallUs/src/app/src/pages/Home/index.tsx
+++ Sprint-4/BetterCallUs/src/app/src/pages/Home/index.tsx
@@ -17,7 +17,70 @@ export default class Home extends Component {
                     <div className='botoes'>
                         
                     <Botao textoBotao="Problemas com conectividade na rede" conteudo={
-                    <div>Conteúdo do Botão 1</div>
+                    <div className='textoproblema'>
+                
+                <div>
+                    <p><strong>Introdução:</strong></p>
+                    <p>A conectividade de rede é essencial nos dias de hoje, tanto para uso pessoal quanto profissional. Quando surgem problemas nessa área, podem surgir frustrações e impactos significativos em diversas atividades. Este artigo aborda os principais desafios relacionados à conectividade na rede e fornece soluções práticas para superá-los.</p>
+                </div>
+                
+                <div>
+                    <p><strong>Identificando o Problema:</strong></p>
+                    <p>O primeiro passo para resolver problemas de conectividade é identificar a origem do issue. Pode ser um problema no hardware, software, configuração da rede ou até mesmo no provedor de serviços. Aqui estão algumas etapas iniciais para diagnosticar o problema:</p>
+
+                    <p><strong>1 - Verifique a Conexão Física:</strong></p>
+                    <p>Certifique-se de que todos os cabos estejam devidamente conectados.</p>
+                    <p>Verifique se há danos visíveis nos cabos ou nos dispositivos de rede.</p>
+                </div>
+                
+                <div>
+                    <p><strong>2 - Reinicie Dispositivos:</strong></p>
+                    <p>Reinicie o roteador, modem e o dispositivo que está enfrentando problemas de conectividade.</p>
+                </div>
+                
+                <div>
+                    <p><strong>3- Verifique as Configurações de Rede:</strong></p>
+                    <p>Garanta que as configurações de rede, como endereço IP e gateway, estejam configuradas corretamente.</p>
+                </div>
+                
+                <div>
+                    <p><strong>Soluções Comuns:</strong></p>
+                    <p><strong>Problemas de Configuração do Roteador:</strong></p>
+                    <p>Acesse a interface de administração do roteador digitando seu endereço IP em um navegador web.</p>
+                    <p>Verifique as configurações de segurança, como senhas e filtros de MAC.</p>
+                </div>
+                
+                <div>
+                    <p><strong>Atualização de Drivers e Firmware:</strong></p>
+                    <p>Certifique-se de que os drivers de rede nos dispositivos estão atualizados.</p>
+                    <p>Verifique se há atualizações de firmware disponíveis para o roteador e modem.</p>
+                </div>
+                
+                <div>
+                    <p><strong>Problemas de Provedor de Serviços:</strong></p>
+                    <p>Entre em contato com o provedor de serviços para verificar se há interrupções na área.</p>
+                    <p>Solicite assistência técnica e forneça detalhes sobre os sintomas.</p>
+                </div>
+                
+                <div>
+                    <p><strong>Firewall e Antivírus:</strong></p>
+                    <p>Desative temporariamente o firewall e o antivírus para ver se eles estão bloqueando a conexão.</p>
+                    <p>Certifique-se de que as configurações de segurança não estão bloqueando o tráfego de rede.</p>
+                </div>
+                
+                <div>
+                    <p><strong>Ferramentas de Diagnóstico:</strong></p>
+                    <p><strong>Ping e Traceroute:</strong></p>
+                    <p>Utilize comandos como "ping" e "traceroute" para diagnosticar a conectividade e identificar onde o problema pode estar ocorrendo.</p>
+                    <p><strong>Utilize Ferramentas Online:</strong></p>
+                    <p>Existem várias ferramentas online que podem ajudar a diagnosticar problemas de conectividade, como testes de velocidade de internet e verificações de ping.</p>
+                </div>
+                
+                <div>
+                    <p><strong>Conclusão:</strong></p>
+                    <p>Resolver problemas de conectividade na rede pode ser desafiador, mas seguindo uma abordagem sistemática, é possível identificar e corrigir a maioria dos problemas...</p>
+                    <p>Caso as soluções sugeridas não resolvam o problema, é recomendável buscar a ajuda de profissionais de TI ou do provedor de serviços de internet para uma assistência mais especializada.</p>
+                </div></div>
                     } />
 
                     <Botao textoBotao="Latência alta frequente" conteudo={
