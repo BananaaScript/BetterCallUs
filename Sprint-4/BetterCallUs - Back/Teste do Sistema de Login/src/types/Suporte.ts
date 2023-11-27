3cc00f47d8737162a5dc18ec7f61872d7f5707c2
Douglas F. Medeiros <80690245+DouglasMedeiros1@users.noreply.github.com>
2023-11-27 01:05:38 -0300

    Exclui arquivos desnecessarios do projeto

--- Sprint-4/BetterCallUs - Back/Teste do Sistema de Login/src/types/Suporte.ts
+++
@@ -1,7 +0,0 @@
-import Conta from "./Conta";
-
-class Suporte extends Conta {
-    constructor(nome: string, cpf: string, senha: string, privilegio: string, email:string) {
-        super(nome, cpf, senha, privilegio, email);
-    }
-}
--- Sprint-4/BetterCallUs Backup/src/BetterCallUs/server/bdSLA.sql
+++
@@ -1,9 +0,0 @@
-create database teste;
-use teste;
-
-CREATE TABLE chamado (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    prioridade VARCHAR(10),
-    area VARCHAR(255),
-    tempoderesposta INT
-);
